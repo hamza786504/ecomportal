f0a3ee87363d32a79bc497c79c00442da06dd811
hamza <hamza786504@gmail.com>
2022-06-11 20:35:28 +0500

    category slider issue resolve try 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,46 @@
-const scrollContainer = document.querySelector(".categories_slider");
+Draggable.create("#slider", {
+    bounds:"#categories_slider",
+//allowNativeTouchScrolling:false,
+    type:"x",
+throwProps: true
+    }
+)
 
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-});
+// TweenMax.set("body", {overflowY:'scroll'});
+
+// var scrollSpeed = 0.5; // half of user scroll
+// var scrollHeight = 10000;
+
+// TweenMax.set("body", {height:scrollHeight, overflowY:'scroll'});
+
+$(document).on("scroll", function (){
+const pixels = $(document).scrollTop();
+TweenMax.to('#slider',1,{x:-0.5 * pixels})
+
+//  '0.5' = half speed of user scroll
+console.log(pixels);
+})
+
+/* This Pen Loads the Following
+
+TweenMax: latest version
+-TweenLite
+-TweenMax
+-TimelineLite
+-TimelineMax
+-CSSPlugin
+-BezierPlugin
+-DirectionalRotationPlugin
+-AttrPlugin
+-RoundPropsPlugin
+-EasePack
+Draggable
+
+** Bonus Plugins **
+-ThrowPropsPlugin
+
+Please use the minimal amount of code
+possible to recreate the problem you
+are experiencing.
+
+*/
